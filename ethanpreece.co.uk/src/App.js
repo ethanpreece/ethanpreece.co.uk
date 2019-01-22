c02b55fa610ccb3ab6f7d4836a9dae79b41e90cb
ethan preece <ethanpreece@ethans-MacBook-Pro.local>
2019-01-22 15:27:34 +0000

    business card working

--- ethanpreece.co.uk/src/App.js
+++ ethanpreece.co.uk/src/App.js
@@ -4,7 +4,24 @@ import "./App.css";
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <div className="grid-container" id="f1_container">
+          <canvas
+            width="900rem"
+            height="450rem"
+            id="f1_card"
+            className="item-a canvasStyle slide-in shadow front face"
+          />
+          <canvas
+            width="900rem"
+            height="450rem"
+            id=""
+            className="item-a canvasStyle slide-in back face"
+          />
+        </div>
+      </div>
+    );
   }
 }
 
